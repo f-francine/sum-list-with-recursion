512e4832d163ff17e1271d3ccb3cd0ef0c3ea061
Francine Guimarães <francinesguimaraes@outlook.com>
2021-08-21 02:01:09 -0300

    feat: add sum_list function

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,6 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
-  end
+  def call(list), do: sum_list(0, list)
+
+  defp sum_list(acc, []), do: acc
+  defp sum_list(acc, [_head | tail]), do: sum_list(acc + 1, tail)
 end
